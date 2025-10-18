29e19db8b05d3375c43610970260e3bcf9eb0107
vicchui <vic.chui383@gmail.com>
2025-10-18 11:11:23 +0800

    paper

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     'use strict';
     console.log('main.js loaded');
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+    //TODO
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+    paper.view.draw();
 })
