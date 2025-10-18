3607236b0b0c7158890cb50e80ad174b1c8b73c4
vicchui <vic.chui383@gmail.com>
2025-10-18 11:57:53 +0800

    circles

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ $(document).ready(function() {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
     //TODO
-    var c = Shape.Circle(200, 200, 50);
-    c.fillColor = 'green';
+    //var c = Shape.Circle(200, 200, 25);
+    //c.fillColor = 'green';
+    
+    var c;
+    for (var x = 25; x < 400; x+=50) {
+        for (var y = 25; y < 400; y+=50) {
+            c = Shape.Circle(x, y, 20);
+            c.fillColor = 'green';
+        }
+    }
+    
+    
     paper.view.draw();
 })
