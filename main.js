c8fd87a1292a053328769c9ce919425763e89f4c
vicchui <vic.chui383@gmail.com>
2025-10-18 12:07:01 +0800

    mouseEvent

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
     //TODO
-    //var c = Shape.Circle(200, 200, 25);
+    //var c = Shape.Circle(25, 25, 25);
     //c.fillColor = 'green';
-    
+    /*
     var c;
     for (var x = 25; x < 400; x+=50) {
         for (var y = 25; y < 400; y+=50) {
@@ -14,7 +14,13 @@ $(document).ready(function() {
             c.fillColor = 'green';
         }
     }
-    
-    
+    */
+    var tool = new Tool();
+    tool.onMouseDown = function(event){
+        //var c = Shape.Circle(event.point.x, event.point.y, 20);
+        var c = Shape.Circle(event.point, 20);
+
+        c.fillColor='green';
+    }    
     paper.view.draw();
 })
